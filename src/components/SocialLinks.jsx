6f515370b07be2557840a232d9a13ab59ebc5107
Emilia Burza <emila.burza@gmail.com>
2024-10-31 17:46:58 +0100

    Add social media icons and links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,13 @@
+import Linkedin from '../assets/linkedin.svg'
+import Behance from '../assets/behance.svg'
+import Github from '../assets/github.svg'
+
 export default function SocialLinks() {
     return(
-        <img src='#' />
+        <>
+            <a href='https://www.linkedin.com/in/emilia-burza/'><img src={Linkedin} /></a>
+            <a href='https://github.com/eburza'><img src={Github} /></a>
+            <a href='https://www.behance.net/emiliaburza'><img src={Behance} /></a>
+        </>
     )
 }
