10eaeefd43d135f01f023dcbedeba25ed37b15d4
Emilia Burza <emila.burza@gmail.com>
2024-11-01 13:56:26 +0100

    Create skills and tools data file

---
+++ src/data/skillsAndTools.js
@@ -0,0 +1,99 @@
+import Adobe from '../assets/skills-and-tools/adobe.svg'
+import Css from '../assets/skills-and-tools/css.svg'
+import Figma from '../assets/skills-and-tools/figma.svg'
+import Firebase from '../assets/skills-and-tools/firebase.svg'
+import Git from '../assets/skills-and-tools/git.svg'
+import Github from '../assets/skills-and-tools/github.svg'
+import Html from '../assets/skills-and-tools/html.svg'
+import Javascript from '../assets/skills-and-tools/javascript.svg'
+import Jquery from '../assets/skills-and-tools/jquery.svg'
+import React from '../assets/skills-and-tools/react.svg'
+import Sass from '../assets/skills-and-tools/sass.svg'
+import Terminal from '../assets/skills-and-tools/terminal.svg'
+import Vscode from '../assets/skills-and-tools/vscode.svg'
+import Accessibility from '../assets/skills-and-tools/accessibility.svg'
+import Responsiveness from '../assets/skills-and-tools/responsiveness.svg'
+
+
+const skillsList = [
+    {
+        name: 'React',
+        tag: 'react',
+        img: {React}
+    },
+    {
+        name: 'JavaScript',
+        tag: 'javascript',
+        img: {Javascript}
+    },
+    {
+        name: 'jQuery',
+        tag: 'jquery',
+        img: {Jquery}
+    },
+    {
+        name: 'Firebase',
+        tag: 'firebase',
+        img: {Firebase}
+    },
+    {
+        name: 'Git',
+        tag: 'git',
+        img: {Git}
+    },
+    {
+        name: 'SASS',
+        tag: 'sass',
+        img: {Sass}
+    },
+    {
+        name: 'CSS3',
+        tag: 'css',
+        img: {Css}
+    },
+    {
+        name: 'HTML5',
+        tag: 'html',
+        img: {Html}
+    },
+    {
+        name: 'Responsive Websites',
+        tag: 'responsiveness',
+        img: {Responsiveness}
+    },
+    {
+        name: 'Web Accessibility',
+        tag: 'accessibility',
+        img: {Accessibility}
+    }
+]
+
+const toolsList = [
+    {
+        name: 'VS Code',
+        tag: 'vscode',
+        img: {Vscode}
+    },
+    {
+        name: 'GitHub',
+        tag: 'github',
+        img: {Github}
+    },
+    {
+        name: 'Terminal',
+        tag: 'terminal',
+        img: {Terminal}
+    },
+    {
+        name: 'Adobe Suite',
+        tag: 'adobe',
+        img: {Adobe}
+    },
+    {
+        name: 'Figma',
+        tag: 'figma',
+        img: {Figma}
+    }
+]
+
+export {skillsList, toolsList}
