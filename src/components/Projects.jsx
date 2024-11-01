0e3b730a0aa5029ca6c3fd1d49c43ab928899a9f
Emilia Burza <emila.burza@gmail.com>
2024-11-01 15:03:28 +0100

    Refactor projects component, add arrow buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,19 @@
 import ProjectListElement from './ProjectListElement'
+import ArrowButton from '../assets/arrow-button.svg'
 
 export default function Projects() {
     return(
-        <>
-            <h1>Projects</h1>
-            <p>
+        <div className='projects-content'>
+            <h1 className='head-title'>Projects</h1>
+            <p className='projects-intro-text'>
                 Explore my frontend development projects built with React.js 
                 and modern web technologies. These works demonstrate my focus 
                 on creating responsive user interfaces and interactive web applications, 
                 while documenting my ongoing journey into full-stack development.
             </p>
             <ProjectListElement />
-            <button>{`<-`}</button>
-            <button>{`->`}</button>
-        </>
+            <button className='previous-project project-list-button'><img src={ArrowButton}/></button>
+            <button className='next-project project-list-button'><img src={ArrowButton}/></button>
+        </div>
     )
 }
