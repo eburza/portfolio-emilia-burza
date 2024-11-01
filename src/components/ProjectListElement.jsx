4faa9f4547a6ac5bded1f424024cf863acbee788
Emilia Burza <emila.burza@gmail.com>
2024-11-01 16:05:22 +0100

    Refactor project element file

--- src/components/ProjectListElement.jsx
+++ src/components/ProjectListElement.jsx
@@ -1,15 +1,16 @@
-// import ProjectListTechElements from './ProjectListTechElements'
+import ProjectListTechElements from './ProjectListTechElements'
 import ArrowButton from '../assets/arrow-button.svg'
 
 export default function ProjectListElement({name, image, imageAlt, summary, tags}) {
     return(
         <div className='project-preview'>
-            <img src={image} alt={imageAlt} />
+            <img src={image} alt={imageAlt} className='project-preview-img'/>
             <h2>{name}</h2>
             <p>{summary}</p>
-            <p>{tags}</p>
-            {/* <ProjectListTechElements /> */}
-            <button><img src={ArrowButton}/></button>
+            <ProjectListTechElements 
+                projectTgas={tags}
+            />
+            <button className='project-preview-button'><img src={ArrowButton}/></button>
         </div>
     )
 }
