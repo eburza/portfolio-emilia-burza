2e12945623ea719823c23e5abc83bc77cef75f74
Emilia Burza <emila.burza@gmail.com>
2024-11-05 14:15:56 +0100

    Add styling to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,24 @@ import NavigationLinks from './NavigationLinks'
 
 export default function Footer() {
     return(
-        <div className='footer'>
-            <div className='footer-info'>
-                <NavigationLinks 
-                    NavClassName="navigation-footer-links"/>
-                <div className='footer-dev-info'>
-                    <p>Designed and Developed with </p>
-                    <img src={Heart} className='footer-heart-img'/>
-                    <p> by Emilia</p>
+        <footer>
+            <div className='footer-content'>
+                <div className='footer-info'>
+                    <NavigationLinks 
+                        NavClassName="navigation-footer-links"/>
+                    <div className='footer-dev-info'>
+                        <p>Designed and Developed with </p>
+                        <img src={Heart} className='footer-heart-img'/>
+                        <p> by Emilia</p>
+                    </div>
+                </div>
+                <div className='personal-card'>
+                    <h2 className='card-title'>Say hey!</h2>
+                    <p className='card-phone'>+48 782 640 208</p>
+                    <p className='card-email'>emila.burza@gmail.com</p>
+                    <SocialLinks />
                 </div>
             </div>
-            <div className='personal-card'>
-                <h2>Say hey!</h2>
-                <p>+48 782 640 208</p>
-                <p>emila.burza@gmail.com</p>
-                <SocialLinks />
-            </div>
-        </div>
+        </footer>
     )
 }
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,10 +4,10 @@ import Github from '../assets/social-media-icons/github.svg'
 
 export default function SocialLinks() {
     return(
-        <>
+        <div className='card-social'>
             <a href='https://www.linkedin.com/in/emilia-burza/'><img src={Linkedin} /></a>
             <a href='https://github.com/eburza'><img src={Github} /></a>
             <a href='https://www.behance.net/emiliaburza'><img src={Behance} /></a>
-        </>
+        </div>
     )
 }
