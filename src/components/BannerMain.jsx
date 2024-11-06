2cb5252e87576655571f140895047932aec4a3c5
Emilia Burza <emila.burza@gmail.com>
2024-11-06 16:11:32 +0100

    Refactor hero component styling file

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -4,8 +4,8 @@ export default function BannerMain() {
     return(
         <header className='hero'>
             <div className='hero-text'>
-                <p className='hero-text-sub'>Hi, I am</p>
-                <h1 className='hero-text-main'>Emilia Burza < br />I am a Graphic Designer <br />turned <span className='underscore'>Frontend Developer</span></h1>
+                <p className='heading-s'>Hi, I am</p>
+                <h1 className='heading-xl'>Emilia Burza < br />I am a Graphic Designer <br />turned <span className='underscore'>Frontend Developer</span></h1>
             </div>
             <img src={MainBannerImg} className='hero-img' alt='Cartoon illustration of a woman developer at work with her laptop and programming tools'/>
         </header>
