08d97ca21f181fb7fc53bf942801c0a80cc52b8d
Emilia Burza <emila.burza@gmail.com>
2024-11-06 16:16:17 +0100

    Refactor about component styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,16 @@ import ImgAbout from '../assets/img-about.png'
 export default function About() {
     return(
         <article className='about-content' id='about'>
-            <h1 className='head-title underscore-elusive'>About me</h1>
+            <h1 className='heading-l underscore-elusive'>About me</h1>
             <div className='about-wrapper'>
                 <img src={ImgAbout} className='about-img' alt='Emilia Burza smiling'/>
                 <div className='about-text-container'>
-                    <p className='about-text text-1'>
+                    <p className='text-paragraph text-1'>
                         As a frontend developer with a focus on React.js, I bring a powerful combination of technical expertise 
                         and design sensibility to web development. My transition from graphic design to code has equipped me with the skills 
                         to create web applications that are not only highly functional but also intuitively user-friendly and visually compelling.
                     </p>
-                    <p className='about-text text-2'>
+                    <p className='text-paragraph text-2'>
                         Currently, I'm dedicated to expanding my full-stack development skills, focusing on building scalable React 
                         applications on the front-end and robust backend systems. I'm deepening my knowledge of server-side technologies 
                         like Node.js and databases, while improving web accessibility and performance. I'm eager to join a team where 
