ff1d6238badb033699a6f8031657db8275bd4a74
Emilia Burza <emila.burza@gmail.com>
2024-11-08 16:18:27 +0100

    Create a file containing a list of project images

---
+++ src/data/projectImagesList.js
@@ -0,0 +1,13 @@
+import TicTacToeMain from '../assets/projects-images/tic-tac-toe/tic-tac-toe-main.png'
+import TicTacToeP1 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-1.png'
+import TicTacToeP2 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-2.png'
+import TicTacToeP3 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-3.png'
+import TicTacToeP4 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-4.png'
+import TicTacToeP5 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-5.png'
+
+const projectTicTacToeImages = {
+    main: TicTacToeMain,
+    previews: [TicTacToeP1, TicTacToeP2, TicTacToeP3, TicTacToeP4, TicTacToeP5]
+}
+
+export { projectTicTacToeImages }
