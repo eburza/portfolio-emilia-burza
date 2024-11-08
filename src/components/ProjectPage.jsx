a89e33161cea7b7998c7c17b89a7b4f78cd17231
Emilia Burza <emila.burza@gmail.com>
2024-11-08 15:19:42 +0100

    Refactor and style the hero section at project page

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import { projectList } from '../data/projectsList'
 import ArrowBack from '../assets/arrow-back.svg'
+import ArrowLink from '../assets/link-arrow.svg'
 
 export default function ProjectPage() {
     const { id } = useParams();
@@ -13,14 +14,50 @@ export default function ProjectPage() {
     
     return (
         <article className='project-page'>
-            <div className='back-to-home'>
-            <Link to={`/`} className='logo'>
-                <img src={ArrowBack} className='arrow-icon'/>
-            </Link>
-            </div>
-            <header className='project-header'>
-                <h1 className='heading-xl project-title'>{project.name}</h1>
-                <p className='text-paragraph project-summary'>{project.summary}</p>
+            <header className='header-project'>
+                <div className='back-to-home'>
+                    <Link to={`/#projects`} className='back-to-home-link'>
+                        <img src={ArrowBack} className='arrow-icon'/>
+                        <p className='text-paragraph-s'>Back to home</p>
+                    </Link>
+                </div>
+                <div className='project-hero'>
+                    <div className='hero-summary'>
+                        <h1 className='heading-xl project-title underscore-water'>{project.name}</h1>
+                        <p className='text-paragraph project-summary'>{project.summary}</p>
+                    </div>
+                    <div className='hero-data'>
+                        <div className='data-links'>
+                            <h3 className='heading-s'>Links</h3>
+                            <a 
+                            href={project.liveLink} 
+                            target='_blank' 
+                            rel='noopener noreferrer'
+                            className='project-link live-link'
+                            >
+                                <p className='text-paragraph'>View Live</p>
+                                <img src={ArrowLink} className='arrow-link-icon'/>
+                            </a>
+                            <a 
+                            href={project.githubLink} 
+                            target='_blank' 
+                            rel='noopener noreferrer'
+                            className='project-link github-link'
+                            >
+                                <p className='text-paragraph'>View Code</p>
+                                <img src={ArrowLink} className='arrow-link-icon'/>
+                            </a>
+                        </div>
+                        <div className='data-tags'>
+                            <h3 className='heading-s'>Technologies used</h3>
+                            <div className='tech-tags'>
+                            {project.tags.map((tag, index) => (
+                                <span key={index} className='tech-tag text-paragraph-s'>{tag}</span>
+                            ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </header>
             
             <div className='project-content'>
@@ -34,34 +71,6 @@ export default function ProjectPage() {
                     <section className='project-description'>
                         <h2 className='heading-l'>About this project</h2>
                         <p className='text-paragraph'>{project.description}</p>
-                    </section>
-                    
-                    <section className='project-tech'>
-                        <h2 className='heading-m'>Technologies Used</h2>
-                        <div className='tech-tags'>
-                            {project.tags.map((tag, index) => (
-                                <span key={index} className='tech-tag'>{tag}</span>
-                            ))}
-                        </div>
-                    </section>
-                    
-                    <section className='project-links'>
-                        <a 
-                            href={project.liveLink} 
-                            target='_blank' 
-                            rel='noopener noreferrer'
-                            className='project-link live-link'
-                        >
-                            View Live
-                        </a>
-                        <a 
-                            href={project.githubLink} 
-                            target='_blank' 
-                            rel='noopener noreferrer'
-                            className='project-link github-link'
-                        >
-                            View Code
-                        </a>
                     </section>
                 </div>
             </div>
