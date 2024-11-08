a912e3da9af4e26cc065cd700db805a138f90c8d
Emilia Burza <emila.burza@gmail.com>
2024-11-08 13:10:54 +0100

    Add link to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import Logo from '../assets/eb-logo.png'
 import NavigationLinks from './NavigationLinks'
 
 export default function Navbar() {
     return (
         <nav className='navigation-bar'>
-            <img src={Logo} className='logo'/>
+            <Link to={`/`} className='logo'>
+                <img src={Logo} className='logo'/>
+            </Link>
             <NavigationLinks 
                 NavClassName="navigation-bar-links"
             />
