c9497bf6c8dda0dd6989602ca4a55e38a5a70838
Emilia Burza <emila.burza@gmail.com>
2024-11-11 18:42:31 +0100

    Add data to project list

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -9,8 +9,9 @@ const projectList = [
         imgAlt: 'Tic-Tac-Toe game preview',
         description: 'This project is a web-based implementation of the classic Tic Tac Toe game. It is designed to provide an engaging and interactive experience for users, allowing them to play against another player or a CPU. The game is built using modern web technologies, ensuring a responsive and visually appealing interface.',
         tags: ['react', 'javascript', 'sass', 'vite'],
-        liveLink: 'https://project1.com',
-        githubLink: 'https://github.com/username/project1',
+        conclusion: `This Tic Tac Toe game project showcases my ability to build interactive web applications using modern technologies. It demonstrates my skills in React, state management, responsive design, and testing. This project is a testament to my problem-solving abilities and attention to detail in creating a polished and user-friendly application`,
+        liveLink: 'https://emilia-burza-tic-tac-toe-game.netlify.app/',
+        githubLink: 'https://github.com/eburza/tic-tac-toe-game',
         images: [
             {
                 src: projectTicTacToeImages.previews[0],
@@ -32,12 +33,71 @@ const projectList = [
                 alt: 'Project screenshot 4',
                 caption: 'Homepage design showcasing the main features'
             },
+        ],
+        features: [
             {
-                src: projectTicTacToeImages.previews[4],
-                alt: 'Project screenshot 5',
-                caption: 'Homepage design showcasing the main features'
+                bold: 'Player vs Player and Player vs CPU Modes',
+                text: 'Users can choose to play against another player or challenge the CPU'
             },
-        ]
+            {
+                bold: 'Responsive Design',
+                text: 'The game is designed to work seamlessly across different devices and screen sizes'
+            },
+            {
+                bold: 'Real-time Updates',
+                text: 'The game state updates in real-time, providing a smooth and interactive user experience'
+            },
+            {
+                bold: 'Score TrackingPlayer vs Player and Player vs CPU Modes',
+                text: 'The application keeps track of the scores for both players and displays the results at the end of each round'
+            },
+            {
+                bold: 'Reset and Restart Options',
+                text: 'Users can reset the game or start a new round at any time'
+            }
+        ],
+        technologies: [
+            {
+                bold: 'React',
+                text: `The game is built using React, a popular JavaScript library for building user interfaces. React's component-based architecture allows for efficient rendering and state management`
+            },
+            {
+                bold: 'Vite',
+                text: 'The project uses Vite as the build tool, providing fast development and optimized production builds'
+            },
+            {
+                bold: 'Sass',
+                text: 'Styles are written in Sass, a powerful CSS preprocessor, allowing for modular and maintainable stylesheets'
+            },
+            {
+                bold: 'Jest and React Testing Library',
+                text: 'The application is thoroughly tested using Jest and React Testing Library to ensure reliability and robustness'
+            }
+        ],
+        highlights: [
+            {
+                bold: 'State Management',
+                text: 'The game state is managed using a custom hook, useGameState, which utilizes the useReducer hook for handling complex state transitions'
+            },
+            {
+                bold: 'Game Logic',
+                text: 'The logic for determining the game winner and handling player moves is encapsulated within the gameReducer function'
+            },
+            {
+                bold: 'Responsive UI',
+                text: 'The UI components are styled using a combination of CSS and Sass, ensuring a consistent look and feel across different devices'
+            }
+        ],
+        challenges: [
+            {
+                bold: 'Handling Game State',
+                text: 'Managing the game state, especially with the CPU logic, was a challenge. This was addressed by carefully structuring the state and using useReducer to handle state transitions efficiently'
+            },
+            {
+                bold: 'Responsive Design',
+                text: 'Ensuring the game looked good on all devices required careful planning and testing. Media queries and flexible layouts were used to achieve this'
+            }
+        ],
     },
     {
         name: 'Project 2',
