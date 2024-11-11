3541e38e465e8562f00f4e9792da5c23d2079094
Emilia Burza <emila.burza@gmail.com>
2024-11-11 18:39:43 +0100

    Update TicTacToe  project images

--- src/data/projectImagesList.js
+++ src/data/projectImagesList.js
@@ -3,11 +3,10 @@ import TicTacToeP1 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-previ
 import TicTacToeP2 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-2.png'
 import TicTacToeP3 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-3.png'
 import TicTacToeP4 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-4.png'
-import TicTacToeP5 from '../assets/projects-images/tic-tac-toe/tic-tac-toe-preview-5.png'
 
 const projectTicTacToeImages = {
     main: TicTacToeMain,
-    previews: [TicTacToeP1, TicTacToeP2, TicTacToeP3, TicTacToeP4, TicTacToeP5]
+    previews: [TicTacToeP1, TicTacToeP2, TicTacToeP3, TicTacToeP4]
 }
 
 export { projectTicTacToeImages }
