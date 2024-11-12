7a24e9620b54bbd533020bdba8e5695b33272a42
Emilia Burza <emila.burza@gmail.com>
2024-11-12 15:20:30 +0100

    Change project preview text styling preferences
    Add truncated text with expansion functionality

--- src/components/ProjectListElement.jsx
+++ src/components/ProjectListElement.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ProjectListTechElements from './ProjectListTechElements'
 import ArrowButton from '../assets/arrow-button.svg'
 
 export default function ProjectListElement({id, name, image, imageAlt, description, tags}) {
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const handleDescriptionClick = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        setIsExpanded(!isExpanded)
+      }
+
     return(
         <Link to={`/project/${id}`}>
             <div className='project-preview'>
@@ -10,11 +19,24 @@ export default function ProjectListElement({id, name, image, imageAlt, descripti
                     <img src={image} alt={imageAlt} className='project-preview-img'/>
                 </div>
                 <h2 className='heading-s'>{name}</h2>
-                <p className='text-paragraph-s project-preview-summary'>{description}</p>
-                <ProjectListTechElements 
-                    projectTgas={tags}
-                />
-                    <button className='project-preview-button'><img src={ArrowButton} className='project-preview-button-img'/></button>
+                <div 
+                className={`project-preview-summary ${isExpanded ? 'expanded' : ''}`}
+                onClick={handleDescriptionClick}
+                role="button"
+                tabIndex={0}
+                >
+                    <p className="text-paragraph-s">
+                        {description}
+                    </p>
+                </div>
+                <div className='tech-tags-container'>
+                    <ProjectListTechElements 
+                        projectTgas={tags}
+                    />
+                </div>
+                <button className='project-preview-button'>
+                    <img src={ArrowButton} className='project-preview-button-img'/>
+                </button>
             </div>
         </Link>
     )
