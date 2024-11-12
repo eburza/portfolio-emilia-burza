63dffb9eaf79bff4dcb9a309e2fea92f4f318ead
Emilia Burza <emila.burza@gmail.com>
2024-11-12 22:22:55 +0100

    Update navigation links

--- src/components/NavigationLinks.jsx
+++ src/components/NavigationLinks.jsx
@@ -1,8 +1,8 @@
 export default function NavigationLinks({NavClassName}) {
     return(
         <ul className={NavClassName}> 
-            <li><a href='#about' className='nav-link text-navigation'>About</a></li>
-            <li><a href='#projects' className='nav-link text-navigation'>Work</a></li>
+            <li><a href='/#about' className='nav-link text-navigation'>About</a></li>
+            <li><a href='/#projects' className='nav-link text-navigation'>Work</a></li>
             <li><a href='mailto:emila.burza@gmail.com' className='nav-link text-navigation'>Contact</a></li>
         </ul>
     )
