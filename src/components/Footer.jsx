653b2f9d96f2384220854488c0cc187e5958aeff
Emilia Burza <emila.burza@gmail.com>
2024-11-22 16:00:45 +0100

    Refactor footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,12 +13,14 @@ export default function Footer() {
                 <div className='footer-dev-info'>
                     <p className='text-paragraph-s'>Designed and Developed with </p>
                     <img src={Heart} className='footer-heart-img'/>
-                    <p className='text-paragraph-s'> by Emilia</p>
+                    <p className='text-paragraph-s'> by Emilia Burza</p>
                 </div>
                 <div className='personal-card'>
                     <h2 className='heading-s card-title'>Say hi!</h2>
                     <p className='text-paragraph-s card-phone'>+48 782 640 208</p>
-                    <p className='text-paragraph-s card-email'>emila.burza@gmail.com</p>
+                    <p className='text-paragraph-s card-email'>
+                        <a href='mailto:emila.burza@gmail.com'>emila.burza@gmail.com</a>
+                    </p>
                     <SocialLinks />
                 </div>
             </div>
