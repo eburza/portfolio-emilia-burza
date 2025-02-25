be726a7ca9ec3c54cd6526ac93724e326412ad08
Emilia Burza <emila.burza@gmail.com>
2025-02-25 23:57:02 +0100

    Update hero section text

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -5,8 +5,8 @@ export default function BannerMain() {
         <header className='hero'>
             <div className='hero-container'>
                 <div className='hero-text'>
-                    <p className='heading-s'>Hi, I'm Emilia Burza</p>
-                    <h1 className='heading-xl'>I mastered pixels as a Graphic Designer, now I master components as a <span className='underscore hero-react-dev'>React Developer</span></h1>
+                    <p className='heading-s'>Hi, I&apos;m Emilia Burza</p>
+                    <h1 className='heading-xl'>I mastered pixels as a Graphic Designer, now I master client and server as a <span className='underscore hero-react-dev'>Full-Stack Developer</span></h1>
                 </div>
                 <img src={MainBannerImg} className='hero-img' alt='Cartoon illustration of a woman developer at work with her laptop and programming tools'/>
             </div>
